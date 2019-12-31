dfb027ccdb4c51f7dcc3db6cb27464c5e3e1e561
Nao YONASHIRO <owan.orisano@gmail.com>
2019-12-31 19:03:12 +0900

    feat: improve performance

--- blocks.go
+++ blocks.go
@@ -2,9 +2,18 @@
 
 package wyhash
 
+import (
+	"encoding/binary"
+)
+
 func consumeBlocks(seed uint64, b []byte) uint64 {
-	for i, size := 0, len(b); i + BlockSize <= size; i += BlockSize {
-		seed = consumeBlock(seed, b)
+	for len(b) > BlockSize {
+		p := b[:BlockSize]
+		p1 := binary.LittleEndian.Uint64(p[0:])
+		p2 := binary.LittleEndian.Uint64(p[8:])
+		p3 := binary.LittleEndian.Uint64(p[16:])
+		p4 := binary.LittleEndian.Uint64(p[24:])
+		seed = mix0(p1, p2, seed) ^ mix1(p3, p4, seed)
 		b = b[BlockSize:]
 	}
 	return seed
